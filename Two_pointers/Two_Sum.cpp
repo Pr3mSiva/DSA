d60f2ac1ca3a11d1d646a54e3ef0110d50f9bf68
Pr3mSiva <work.premsiva@gmail.com>
2026-09-08 21:48:36 +0530

    Update Leetcode Version

--- Two_pointers/Two_Sum.cpp
+++ Two_pointers/Two_Sum.cpp
@@ -53,3 +53,30 @@ int main() {
     }
     return 0;
 }
+
+/*
+LeetCode Version
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            int currentSum = numbers[left] + numbers[right];
+
+            if (currentSum == target) {
+                return {left + 1, right + 1};
+            }
+            else if (currentSum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return {};
+    }
+};
+*/
